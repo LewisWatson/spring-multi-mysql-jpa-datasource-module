6d9a85b31c1197e3d0b3bb9db285370d075df605
Lewis Watson <l.watson@sensewhere.com>
2018-02-26 15:28:43 +0000

    add travis config

--- multi-module-app/src/main/java/com/example/multi/mysql/data/sources/Controller.java
+++ multi-module-app/src/main/java/com/example/multi/mysql/data/sources/Controller.java
@@ -5,22 +5,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-//import com.example.multi.mysql.data.sources.module1.Module1;
+import com.example.multi.mysql.data.sources.module1.Module1;
 import com.example.multi.mysql.data.sources.module2.Module2;
 
 @RestController
 @RequestMapping(path = "/")
 public class Controller {
 
-//  @Autowired
-//  private Module1 module1;
+  @Autowired
+  private Module1 module1;
 
   @Autowired
   private Module2 module2;
 
   @RequestMapping(method = RequestMethod.POST, produces = "application/json")
   void post(@RequestBody Integer request) {
-//    module1.doSomething();
+    module1.doSomething();
     module2.doSomethineElse();
   }
 }
--- multi-module-app/src/main/java/com/example/multi/mysql/data/sources/ControllerConfig.java
+++ multi-module-app/src/main/java/com/example/multi/mysql/data/sources/ControllerConfig.java
@@ -1,10 +1,23 @@
 package com.example.multi.mysql.data.sources;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import com.example.multi.mysql.data.sources.module1.jpa.JpaModule1Config;
-import com.example.multi.mysql.data.sources.module2.jpa.JpaModule2Config;
+import com.example.multi.mysql.data.sources.module1.Module1;
+import com.example.multi.mysql.data.sources.module1.jpa.JpaModule1;
+import com.example.multi.mysql.data.sources.module2.Module2;
+import com.example.multi.mysql.data.sources.module2.jpa.JpaModule2;
 
 @Configuration
-@Import({JpaModule1Config.class, JpaModule2Config.class})
-public class ControllerConfig {}
+public class ControllerConfig {
+  
+  @Bean
+  public Module1 module1() {
+    return new JpaModule1();
+  }
+  
+  @Bean
+  public Module2 module2() {
+    return new JpaModule2();
+  }
+  
+}
